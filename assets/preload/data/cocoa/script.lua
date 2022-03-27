541c780093d23118b47328f62159de16755fa57b
ARAS AKGÜL <94735664+aras123457@users.noreply.github.com>
2022-03-27 19:42:20 +0300

    Create script.lua

---
+++ assets/preload/data/cocoa/script.lua
@@ -0,0 +1,14 @@
+
+    setProperty('timeBarBG.visible', false);
+
+    setProperty('timeBar.visible', false);
+
+For Source:
+
+In PlayState (1.5.2) on line 898:
+
+timeBarBG.visible = false;
+
+And in line 910:
+
+timeBar.visible = showTime;
